e84bdd6f2444080f6562a92f287fe5cb508af505
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-03-17 17:22:10 +0100

    solve svg loading bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
-const title = <h1>A beutifull title</h1>
+// to use svg in React you need to 
+// - minify the file https://www.svgminify.com/
+// or - more convoluted wys https://blog.logrocket.com/how-to-use-svgs-in-react/
+import burger from "./assets/burger.svg"
+
+const theId = 'title'
+const title = <h1 id={theId}>A beutifull title</h1>
 
 function App() {
   return (
     <div className="App">
       { title }
+      <img src={burger} alt='menu icon' style={{width: '20px'}}/>
+      
     </div>
   );
 }
