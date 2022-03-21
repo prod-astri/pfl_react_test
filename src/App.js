03ccb7c4529b4426e6fedf1135e5ba6961b43a01
prod.astri <58519454+prod-astri@users.noreply.github.com>
2022-03-21 14:39:29 +0100

    add project-data json and display prjcts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import { useState } from 'react'
-// to use svg in React you need to 
-// - minify the file https://www.svgminify.com/
-// or - more convoluted ways https://blog.logrocket.com/how-to-use-svgs-in-react/
+
 import Header from './components/Header';
 import ProjectsList from './components/ProjectsList';
 import HeaderButtons from './components/HeaderButtons';
 
+
+
 function App() {
-  const [darkTheme, setDarkTheme] = useState(false);
+ 
+  const [theme, setTheme] = useState('light');
   const [isWide, setIsWide] = useState(false);
 
-  const toggleTheme = e => setDarkTheme(!darkTheme);
+  const toggleTheme = e => setTheme(theme === 'light' ?  'dark' : 'light');
 
   const handleResize = () => {
     setIsWide(window.innerWidth < 600 ? false : true)
@@ -19,14 +20,12 @@ function App() {
   }
   window.addEventListener("resize", handleResize)
 
-
-
   return (
-    <div className={"App " + (darkTheme ? 'dark' : 'light')}>
+    <div className={"App " + (theme)}>
       <Header >
         <HeaderButtons isWide={isWide} />
-        <button className={darkTheme ? 'light' : 'dark'} onClick={toggleTheme}>
-          {darkTheme ? 'set light theme' : 'set dark theme'}
+        <button className={theme} onClick={toggleTheme}>
+          {theme === 'dark' ? "set light theme" : "set dark theme" }
         </button>
       </Header>
       <ProjectsList />
--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
+import projectsData from "../projects-data.json"
 
-export default function ProjectsList(props) {
+export default function ProjectsList() {
+    const [projects] = useState(projectsData);
+
     return (
         // using View instead of a div already changes the appearence
         <View style={styles.container}>
-            <h2> Projects </h2>
-            {props.children}
+            <h2> Projects List</h2>
+            {projects.map(project => {
+                return (
+                    <div key={project._id}>
+                        <h3>{project.name}</h3>
+                        {project.locations.map(location => {
+                           return <small key={project._id + location}>{location}</small>
+                        })}
+                    </div>
+                )
+            })}
         </View>
     )
 }
@@ -14,5 +26,5 @@ export default function ProjectsList(props) {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "#ffeeee"
-      }
+    }
 });
